38000c904bb4d19b9cf8530a0cefc454c2ceee6a
healthykim <bsbs8645@snu.ac.kr>
2023-02-03 22:19:17 +0900

    chore: Add comment

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/*
+문제 1과 3에서 "ETH 혹은 ERC20을 받는" 이라는 표현이 있습니다.
+1) ETH를 받는 Contract 혹은 ERC20을 받는 Contract를 구현
+2) ETH와 ERC20 모두 받을 수 있는 Contract를 구현
+이렇게 두 가지로 해석된다고 생각해서, 1번은 2)로 구현하였고 3번은 1)(ETH의 경우)로 구현하였습니다.
+만약 둘 중 하나를 의도한 것이라면 1번을 3번같이 혹은 3번을 1번같이 바꾸면 되고, 그렇게 하기 편하게 구현했기 때문에 변경은 간단할 것 같습니다.
+*/
+
 contract Bank {
     struct account {
         uint256 balance;
