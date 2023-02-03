781442732ee4df61dc75c967ee0501be1144fcd3
healthykim <bsbs8645@snu.ac.kr>
2023-02-03 22:07:38 +0900

    feat(problem3): Implement deposit function in ECCVerifier.sol

--- contracts/ECCVerifier.sol
+++ contracts/ECCVerifier.sol
@@ -8,6 +8,17 @@ contract ECCVerifier {
         // 
     }
 
+    struct account {
+        uint balance;
+        address owner;
+    }
+
+    mapping (bytes32 => account) private accounts;
+
+    event DEPOSIT(address signer, uint256 amount);
+    event WITHDRAW(address signer, uint256 amount);
+
+
     // signature hash => signer address
     function withdraw(bytes32 signatureHash) public {
         // if verified....mag.sender..
@@ -21,12 +32,11 @@ contract ECCVerifier {
         bytes32 r;
         bytes32 s;
         uint8 v;
-				bool valid;
+
+        require(msg.value > 0);
 
         //Check the signature length
-        if (signature.length != 65) {
-            valid = false;
-        }
+        require(signature.length == 65);
 
         // Divide the signature in r, s and v variables
         assembly {
@@ -34,19 +44,26 @@ contract ECCVerifier {
             s := mload(add(signature, 64))
             v := byte(0, mload(add(signature, 96)))
         }
-
-        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        
         if (v < 27) {
             v += 27;
         }
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        require(v == 27 || v == 28);
 
         // If the version is correct return the signer address
-        if (v != 27 && v != 28) {
-					valid = false;
-        } else {
-            // solium-disable-next-line arg-overflow
-            address signer = ecrecover(message, v, r, s);
-            // ... something
-        }
+        // solium-disable-next-line arg-overflow
+        address signer = recoverAddress(message, v, r, s);
+        accounts[keccak256(abi.encodePacked(signature))].balance += msg.value;
+        accounts[keccak256(abi.encodePacked(signature))].owner = signer;
+        
+        emit DEPOSIT(signer, msg.value);
+    }
+
+
+    function recoverAddress(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public pure returns(address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, msgHash));
+        return ecrecover(prefixedHash, v, r, s);
     }
 }
