9197ac8b907596bae1414262c77b06df60371ca5
healthykim <bsbs8645@snu.ac.kr>
2023-02-03 21:53:00 +0900

    feat(problem2): Implement enter function in Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,15 +5,21 @@ contract Lottery{
     address public manager;
     address[] public players;
 
+    mapping (address => uint8) public entryCount;
+    event WINNER(uint index, address player1, address player2, address player3, uint amount);
+
     function lottery() public {
         manager = msg.sender;
     }
     function enter() public payable{
         require(msg.value > .01 ether);
+        require(entryCount[msg.sender] < 2, "Cannot enter more than 3 times");
+
         players.push(msg.sender);
+        entryCount[msg.sender] += 1;
     }
     function random() private view returns(uint){
-        return  uint (keccak256(abi.encode(block.timestamp,  players)));
+        return uint(keccak256(abi.encode(block.timestamp,  players)));
     }
     function pickWinner() public restricted{
         uint index = random() % players.length;
